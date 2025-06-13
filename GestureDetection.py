566d2b1b98bd66c53f49a4be5753ca9657a71537
Bastian Weiler <bastian.weiler04@gmail.com>
2025-06-13 21:58:11 +0200

    enhanced performance of script

--- GestureDetection.py
+++ GestureDetection.py
@@ -4,17 +4,19 @@ from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
 import time
 
-value = None  # track last gesture to avoid repeats
 
 def save_result(result, output_image, timestamp_ms):
     global value
-    try:
-        if result.gestures and result.gestures[0][0].category_name != value:
-            value = result.gestures[0][0].category_name
-            print("Gesture:", value)
-    except Exception as e:
-        pass
+    
+    if not result.gestures:
+        return
+    
+    if result.gestures[0][0].category_name != value:
+        value = result.gestures[0][0].category_name
+        print("Gesture:", value)
+        
 
+value = None  # track last gesture to avoid repeats
 # Load model
 BASE = python.BaseOptions("gesture_recognizer.task")
 OPTIONS = vision.GestureRecognizerOptions(
@@ -29,26 +31,33 @@ TITLE_CAPTURE = "Capture"
 CAP = cv.VideoCapture(0)
 CAP.set(cv.CAP_PROP_FRAME_WIDTH, 320)
 CAP.set(cv.CAP_PROP_FRAME_HEIGHT, 240)
-cv.namedWindow(TITLE_CAPTURE, cv.WINDOW_AUTOSIZE)
+#cv.namedWindow(TITLE_CAPTURE, cv.WINDOW_AUTOSIZE)
 
+frame_counter = 0
+process_every_n = CAP.get(cv.CAP_PROP_FPS) // 2
+print(process_every_n)
 # Frame loop
 while True:
     ret, image = CAP.read()
     if not ret:
         break
 
-    cv.imshow(TITLE_CAPTURE, image)
+    frame_counter += 1
+    #cv.imshow(TITLE_CAPTURE, image)
+    
+    if frame_counter % process_every_n == 0:
+        rgb = cv.cvtColor(image, cv.COLOR_BGR2RGB)
+        mp_image = mp.Image(
+            image_format=mp.ImageFormat.SRGB,
+            data=rgb
+        )
+        timestamp = int(time.time() * 1000)
+        RECOGNIZER.recognize_async(mp_image, timestamp)
 
-    rgb = cv.cvtColor(image, cv.COLOR_BGR2RGB)
-    mp_image = mp.Image(
-        image_format=mp.ImageFormat.SRGB,
-        data=rgb
-    )
-    timestamp = int(time.time() * 1000)
-    RECOGNIZER.recognize_async(mp_image, timestamp)
-
-    if cv.waitKey(10) & 0xFF == ord('q'):
+    key = cv.waitKey(10)
+    if key == ord('q'):
         break
 
+RECOGNIZER.close()
 CAP.release()
 cv.destroyAllWindows()
