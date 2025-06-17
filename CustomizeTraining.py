8a42d0fb9f67560620f07f58d2d81985bbe3a49f
Bastian Weiler <bastian.weiler04@gmail.com>
2025-06-17 15:59:20 +0200

    added training script for customizing model

---
+++ CustomizeTraining.py
@@ -0,0 +1,25 @@
+from mediapipe_model_maker import gesture_recognizer
+
+data = gesture_recognizer.Dataset.from_folder(
+    dirname="./gesture_dataset",
+    hparams=gesture_recognizer.HandDataPreprocessingParams()
+)
+train_data, rest_data = data.split(0.8)
+validation_data, test_data = rest_data.split(0.5)
+
+hparams = gesture_recognizer.HParams(
+    export_dir="exported_model",
+    epochs=100,
+    learning_rate=0.01
+)
+options = gesture_recognizer.GestureRecognizerOptions(hparams=hparams)
+model = gesture_recognizer.GestureRecognizer.create(
+    train_data=train_data,
+    validation_data=validation_data,
+    options=options
+)
+
+loss, acc = model.evaluate(test_data, batch_size=1)
+print(f"Test loss:{loss}, Test accuracy:{acc}")
+
+model.export_model()
