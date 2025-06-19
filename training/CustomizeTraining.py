f4a347bc517e251e10e9b7e2f14b670d581407fd
Bastian Weiler <bastian.weiler04@gmail.com>
2025-06-19 11:06:40 +0200

    setup training project

--- training/CustomizeTraining.py
+++ training/CustomizeTraining.py
@@ -1,16 +1,18 @@
 from mediapipe_model_maker import gesture_recognizer
 
 data = gesture_recognizer.Dataset.from_folder(
-    dirname="./gesture_dataset",
-    hparams=gesture_recognizer.HandDataPreprocessingParams()
+    dirname="./training/gestures-dataset",
+    hparams=gesture_recognizer.HandDataPreprocessingParams(
+        shuffle=True
+    )
 )
 train_data, rest_data = data.split(0.8)
 validation_data, test_data = rest_data.split(0.5)
 
 hparams = gesture_recognizer.HParams(
-    export_dir="exported_model",
+    export_dir="./training/exported_model",
     epochs=100,
-    learning_rate=0.01
+    learning_rate=0.0005
 )
 options = gesture_recognizer.GestureRecognizerOptions(hparams=hparams)
 model = gesture_recognizer.GestureRecognizer.create(
