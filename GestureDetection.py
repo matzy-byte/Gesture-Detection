93e85cbad3a0aea70c90420d58b8e9b38745bfef
Bastian Weiler <bastian.weiler04@gmail.com>
2025-06-25 16:50:19 +0200

    setup proper mqtt support

--- GestureDetection.py
+++ GestureDetection.py
@@ -1,64 +1,24 @@
-import paho.mqtt.publish as publish
+from paho.mqtt import client as mqtt_client
 import cv2 as cv
 import mediapipe as mp
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
 import time
-import math
 
 
-def get_distance(p1, p2):
-    return math.sqrt(sum((a - b) ** 2 for a, b in zip(p1, p2)))
+MQTT_CLIENT_ID = "Gesture-Detector"
+MQTT_BROKER = ""
+MQTT_PORT = -1
+USERNAME = ""
+PASSWORD = ""
+TOPIC = ""
 
-def save_result(result, output_image, timestamp_ms):
-    global HOST
-    global value
-
-    if not result.gestures:
-        value["category"] = "none"
-        value["landmark"] = (0, 0, 0)
-        publish.single("gesture", value["category"], 2, hostname=HOST)
-        return
-
-    category = result.gestures[0][0].category_name
-    if not category or category == "none":
-        value["category"] = "none"
-        value["landmark"] = (0, 0, 0)
-        publish.single("gesture", value["category"], 2, hostname=HOST)
-        return
-
-    landmark = (
-        result.hand_landmarks[0][0].x,
-        result.hand_landmarks[0][0].y,
-        result.hand_landmarks[0][0].z
-    )
-    if category != value["category"]:
-        value["category"] = category
-        value["landmark"] = landmark
-        print("Gesture:", category)
-        publish.single("gesture", value["category"], 2, hostname=HOST)
-        return
-        
-
-HOST = "localhost"
-BASE = python.BaseOptions("gesture_recognizer.task")
-OPTIONS = vision.GestureRecognizerOptions(
-    base_options=BASE,
-    running_mode=vision.RunningMode.LIVE_STREAM,
-    result_callback=save_result
-)
-RECOGNIZER = vision.GestureRecognizer.create_from_options(OPTIONS)
-value = {
-    "category": "none",
-    "landmark": (0, 0, 0)
-}
+CAP = cv.VideoCapture(0)
+CAP.set(cv.CAP_PROP_FRAME_WIDTH, 320)
+CAP.set(cv.CAP_PROP_FRAME_HEIGHT, 240)
 
 
-def main():
-    CAP = cv.VideoCapture(0)
-    CAP.set(cv.CAP_PROP_FRAME_WIDTH, 320)
-    CAP.set(cv.CAP_PROP_FRAME_HEIGHT, 240)
-
+def analyze_camera_ouput():
     frame_counter = 0
     PROCESS_EVERY_N = CAP.get(cv.CAP_PROP_FPS) // 2
 
@@ -80,6 +40,65 @@ def main():
 
     RECOGNIZER.close()
     CAP.release()
+
+
+def save_result(result, output_image, timestamp_ms):
+    global TOPIC
+    global client
+    global value
+
+    if not result.gestures:
+        value["category"] = "none"
+        value["landmark"] = (0, 0, 0)
+        client.publish(TOPIC, value["category"])
+        return
+
+    category = result.gestures[0][0].category_name
+    if not category or category == "none":
+        value["category"] = "none"
+        value["landmark"] = (0, 0, 0)
+        client.publish(TOPIC, value["category"])
+        return
+
+    landmark = (
+        result.hand_landmarks[0][0].x,
+        result.hand_landmarks[0][0].y,
+        result.hand_landmarks[0][0].z
+    )
+    if category != value["category"]:
+        value["category"] = category
+        value["landmark"] = landmark
+        print("Gesture:", category)
+        client.publish(TOPIC, value["category"])
+        return
+        
+
+BASE = python.BaseOptions("gesture_recognizer.task")
+OPTIONS = vision.GestureRecognizerOptions(
+    base_options=BASE,
+    running_mode=vision.RunningMode.LIVE_STREAM,
+    result_callback=save_result
+)
+RECOGNIZER = vision.GestureRecognizer.create_from_options(OPTIONS)
+value = {
+    "category": "none",
+    "landmark": (0, 0, 0)
+}
+
+
+def on_connect(client, userdata, flags, reason_code):
+    if (reason_code == 0):
+        analyze_camera_ouput()
+    else:
+        print("Not Connected!")
+    
+
+
+def main():
+    client = mqtt_client.Client(client_id=MQTT_CLIENT_ID)
+    client.username_pw_set(username=USERNAME, password=PASSWORD)
+    client.on_connect = on_connect
+    client.loop_forever()
 
 
 if __name__ == "__main__":
